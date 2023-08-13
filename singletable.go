f8cedef1e7037f467509653ad6bf2d941834f27c
Elad Gabay <elad.gabay@wiz.io>
2023-08-13 20:14:37 +0300

    Enhance Support for Larger Datasets and Buckets in Encoding
    This commit improves encoding by enabling the handling of number of items and buckets exceeding max(uint32). Formerly, the encoding used uint32 for counts, but the filter structure already supported larger values using uint.
    Until now, the filter partially supported larger datasets, not all the buckets were utilized, note to the change in `generateIndexTagHash`, `altIndex` and `indexHash`.
    
    Now, all references to bucket indices and item counts explicitly use uint64. A new encoding format accommodates larger filter.
    To distinguish between legacy (up to max(uint32) items) and the new format, a prefix marker is introduced.
    
    Decoding seamlessly supports both formats.
    The encode method takes a legacy boolean parameter for gradual adoption.

--- singletable.go
+++ singletable.go
@@ -16,11 +16,12 @@ import (
 // SingleTable the most naive table implementation: one huge bit array
 type SingleTable struct {
 	kTagsPerBucket uint
-	numBuckets     uint
+	numBuckets     uint64
 	bitsPerTag     uint
 	tagMask        uint32
 	bucket         []byte
-	len            uint
+	len            uint64
+	totalSlots     uint64
 }
 
 // NewSingleTable return a singleTable
@@ -29,34 +30,35 @@ func NewSingleTable() *SingleTable {
 }
 
 // Init init table
-func (t *SingleTable) Init(tagsPerBucket, bitsPerTag, num uint, initialBucketsHint []byte) error {
+func (t *SingleTable) Init(tagsPerBucket, bitsPerTag uint, num uint64, initialBucketsHint []byte) error {
 	t.bitsPerTag = bitsPerTag
 	t.numBuckets = num
 	t.kTagsPerBucket = tagsPerBucket
 
 	t.tagMask = (1 << bitsPerTag) - 1
-	t.len = (t.bitsPerTag*t.kTagsPerBucket*t.numBuckets + 7) >> 3
+	t.len = (uint64(t.bitsPerTag)*uint64(t.kTagsPerBucket)*t.numBuckets + 7) >> 3
 	buckets, err := getBucketsFromHint(initialBucketsHint, t.len)
 	if err != nil {
 		return err
 	}
 	t.bucket = buckets
+	t.totalSlots = uint64(t.kTagsPerBucket) * t.numBuckets
 	return nil
 }
 
 // NumBuckets return num of table buckets
-func (t *SingleTable) NumBuckets() uint {
+func (t *SingleTable) NumBuckets() uint64 {
 	return t.numBuckets
 }
 
 // SizeInBytes return bytes occupancy of table
-func (t *SingleTable) SizeInBytes() uint {
+func (t *SingleTable) SizeInBytes() uint64 {
 	return t.len
 }
 
 // SizeInTags return num of tags that table can store
-func (t *SingleTable) SizeInTags() uint {
-	return t.kTagsPerBucket * t.numBuckets
+func (t *SingleTable) SizeInTags() uint64 {
+	return t.totalSlots
 }
 
 // BitsPerItem return bits occupancy per item of table
@@ -65,8 +67,8 @@ func (t *SingleTable) BitsPerItem() uint {
 }
 
 // ReadTag read tag from bucket(i,j)
-func (t *SingleTable) ReadTag(i, j uint) uint32 {
-	pos := (i*t.bitsPerTag*t.kTagsPerBucket + t.bitsPerTag*j) / bitsPerByte
+func (t *SingleTable) ReadTag(i, j uint64) uint32 {
+	pos := (i*uint64(t.bitsPerTag)*uint64(t.kTagsPerBucket) + uint64(t.bitsPerTag)*j) / bitsPerByte
 	var tag uint32
 	/* following code only works for little-endian */
 	switch t.bitsPerTag {
@@ -89,12 +91,12 @@ func (t *SingleTable) ReadTag(i, j uint) uint32 {
 	return tag & t.tagMask
 }
 
-func (t *SingleTable) readOutBytes(i, j, pos uint) uint32 {
-	rShift := (i*t.bitsPerTag*t.kTagsPerBucket + t.bitsPerTag*j) & (bitsPerByte - 1)
+func (t *SingleTable) readOutBytes(i, j, pos uint64) uint32 {
+	rShift := (i*uint64(t.bitsPerTag)*uint64(t.kTagsPerBucket) + uint64(t.bitsPerTag)*j) & (bitsPerByte - 1)
 	// tag is max 32bit, so max occupies 5 bytes
-	kBytes := (rShift + t.bitsPerTag + 7) / bitsPerByte
+	kBytes := (rShift + uint64(t.bitsPerTag) + 7) / bitsPerByte
 	var tmp uint64
-	for k := uint(0); k < kBytes; k++ {
+	for k := uint64(0); k < kBytes; k++ {
 		tmp |= uint64(t.bucket[pos+k]) << (bitsPerByte * k)
 	}
 	tmp >>= rShift
@@ -102,8 +104,8 @@ func (t *SingleTable) readOutBytes(i, j, pos uint) uint32 {
 }
 
 // WriteTag write tag into bucket(i,j)
-func (t *SingleTable) WriteTag(i, j uint, n uint32) {
-	pos := (i*t.bitsPerTag*t.kTagsPerBucket + t.bitsPerTag*j) / bitsPerByte
+func (t *SingleTable) WriteTag(i, j uint64, n uint32) {
+	pos := (i*uint64(t.bitsPerTag)*uint64(t.kTagsPerBucket) + uint64(t.bitsPerTag)*j) / bitsPerByte
 	tag := n & t.tagMask
 	/* following code only works for little-endian */
 	switch t.bitsPerTag {
@@ -146,11 +148,11 @@ func (t *SingleTable) WriteTag(i, j uint, n uint32) {
 	}
 }
 
-func (t *SingleTable) writeInBytes(i, j, pos uint, tag uint32) {
-	rShift := (i*t.bitsPerTag*t.kTagsPerBucket + t.bitsPerTag*j) & (bitsPerByte - 1)
-	lShift := (rShift + t.bitsPerTag) & (bitsPerByte - 1)
+func (t *SingleTable) writeInBytes(i, j, pos uint64, tag uint32) {
+	rShift := (i*uint64(t.bitsPerTag)*uint64(t.kTagsPerBucket) + uint64(t.bitsPerTag)*j) & (bitsPerByte - 1)
+	lShift := (rShift + uint64(t.bitsPerTag)) & (bitsPerByte - 1)
 	// tag is max 32bit, so max occupies 5 bytes
-	kBytes := (rShift + t.bitsPerTag + 7) / bitsPerByte
+	kBytes := (rShift + uint64(t.bitsPerTag) + 7) / bitsPerByte
 
 	rMask := uint8(0xff) >> (bitsPerByte - rShift)
 	lMask := uint8(0xff) << lShift
@@ -163,15 +165,15 @@ func (t *SingleTable) writeInBytes(i, j, pos uint, tag uint32) {
 	tmp |= uint64(t.bucket[pos+end]&lMask) << (end * bitsPerByte)
 	tmp |= uint64(tag) << rShift
 
-	for k := uint(0); k < kBytes; k++ {
+	for k := uint64(0); k < kBytes; k++ {
 		t.bucket[pos+k] = byte(tmp >> (k * bitsPerByte))
 	}
 }
 
 // FindTagInBuckets find if tag in bucket i1 i2
-func (t *SingleTable) FindTagInBuckets(i1, i2 uint, tag uint32) bool {
-	var j uint
-	for j = 0; j < t.kTagsPerBucket; j++ {
+func (t *SingleTable) FindTagInBuckets(i1, i2 uint64, tag uint32) bool {
+	var j uint64
+	for j = 0; j < uint64(t.kTagsPerBucket); j++ {
 		if t.ReadTag(i1, j) == tag || t.ReadTag(i2, j) == tag {
 			return true
 		}
@@ -180,9 +182,9 @@ func (t *SingleTable) FindTagInBuckets(i1, i2 uint, tag uint32) bool {
 }
 
 // DeleteTagFromBucket delete tag from bucket i
-func (t *SingleTable) DeleteTagFromBucket(i uint, tag uint32) bool {
-	var j uint
-	for j = 0; j < t.kTagsPerBucket; j++ {
+func (t *SingleTable) DeleteTagFromBucket(i uint64, tag uint32) bool {
+	var j uint64
+	for j = 0; j < uint64(t.kTagsPerBucket); j++ {
 		if t.ReadTag(i, j) == tag {
 			t.WriteTag(i, j, 0)
 			return true
@@ -192,16 +194,16 @@ func (t *SingleTable) DeleteTagFromBucket(i uint, tag uint32) bool {
 }
 
 // InsertTagToBucket insert tag into bucket i
-func (t *SingleTable) InsertTagToBucket(i uint, tag uint32, kickOut bool, oldTag *uint32) bool {
-	var j uint
-	for j = 0; j < t.kTagsPerBucket; j++ {
+func (t *SingleTable) InsertTagToBucket(i uint64, tag uint32, kickOut bool, oldTag *uint32) bool {
+	var j uint64
+	for j = 0; j < uint64(t.kTagsPerBucket); j++ {
 		if t.ReadTag(i, j) == 0 {
 			t.WriteTag(i, j, tag)
 			return true
 		}
 	}
 	if kickOut {
-		r := uint(rand.Int31()) % t.kTagsPerBucket
+		r := uint64(uint(rand.Int31()) % t.kTagsPerBucket)
 		*oldTag = t.ReadTag(i, r)
 		t.WriteTag(i, r, tag)
 	}
@@ -224,22 +226,46 @@ func (t *SingleTable) Info() string {
 		t.bitsPerTag, t.kTagsPerBucket, t.numBuckets, t.SizeInTags())
 }
 
-const singleTableMetadataSize = 3 + bytesPerUint32
+const (
+	singleTableMetadataSize       = 3 + bytesPerUint64
+	singleTableMetadataSizeLegacy = 3 + bytesPerUint32
+)
 
-// Encode returns a byte slice representing a TableBucket
-func (t *SingleTable) Reader() (io.Reader, uint) {
-	var metadata [singleTableMetadataSize]byte
+// Reader returns a reader representing a TableBucket
+func (t *SingleTable) Reader(legacy bool) (io.Reader, uint64) {
+	var metadata []byte
+	if legacy {
+		metadata = make([]byte, singleTableMetadataSizeLegacy)
+	} else {
+		metadata = make([]byte, singleTableMetadataSize)
+	}
+
 	metadata[0] = uint8(TableTypeSingle)
 	metadata[1] = uint8(t.kTagsPerBucket)
 	metadata[2] = uint8(t.bitsPerTag)
-	binary.LittleEndian.PutUint32(metadata[3:], uint32(t.numBuckets))
-	return io.MultiReader(bytes.NewReader(metadata[:]), bytes.NewReader(t.bucket)), uint(len(metadata) + len(t.bucket))
+
+	if legacy {
+		binary.LittleEndian.PutUint32(metadata[3:], uint32(t.numBuckets))
+	} else {
+		binary.LittleEndian.PutUint64(metadata[3:], t.numBuckets)
+	}
+
+	return io.MultiReader(bytes.NewReader(metadata[:]), bytes.NewReader(t.bucket)), uint64(len(metadata)) + uint64(len(t.bucket))
 }
 
 // Decode parse a byte slice into a TableBucket
-func (t *SingleTable) Decode(b []byte) error {
+func (t *SingleTable) Decode(legacy bool, b []byte) error {
 	tagsPerBucket := uint(b[1])
 	bitsPerTag := uint(b[2])
-	numBuckets := uint(binary.LittleEndian.Uint32(b[3:]))
-	return t.Init(tagsPerBucket, bitsPerTag, numBuckets, b[7:])
+
+	var numBuckets, offset uint64
+	if legacy {
+		numBuckets = uint64(binary.LittleEndian.Uint32(b[3:]))
+		offset = singleTableMetadataSizeLegacy
+	} else {
+		numBuckets = binary.LittleEndian.Uint64(b[3:])
+		offset = singleTableMetadataSize
+	}
+
+	return t.Init(tagsPerBucket, bitsPerTag, numBuckets, b[offset:])
 }
